d73254d164292901fca8d73ca69ff96c65a30c6c
Pascal Precht <pascal.precht@googlemail.com>
2015-12-21 23:46:26 +0100

    WIP SAVEPOINT

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,6 @@
 (function () {
   'use strict';
 
-  var querySelector = document.querySelector.bind(document);
-
-  var navdrawerContainer = querySelector('.thtrm-l-navdrawer');
-  var body = document.body;
-  var appbarElement = querySelector('.thtrm-l-header');
-  var menuBtn = querySelector('.thtrm-l-header__menu-btn');
-  var main = querySelector('.thtrm-l-main');
-
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
-
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
-
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
-
   // Check to make sure service workers are supported in the current browser,
   // and that the current page is accessed from a secure origin. Using a
   // service worker from an insecure origin will trigger JS console errors. See
